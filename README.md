08606a2df6876c75da1fa7c045764596bc2bb5aa
André Pedersen <andrped94@gmail.com>
2023-10-18 11:25:27 +0200

    Developed working app

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # custom-chatbot-streamlit
+
+This application demonstrates how to setup a simple ChatBot with Azure OpenAI and StreamLit.
+The ChatBot enables you to talk with your own data - in this case, to learn about André's research.
+
+## Getting Started
+
+1.Setup virtual environment and install dependencies:
+```
+python -m venv venv/
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+2. Download test data
+
+## 
+
--- app.py
+++ app.py
@@ -1,27 +1,51 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext
-from llama_index.llms import OpenAI
-import openai
+from llama_index import VectorStoreIndex, ServiceContext, set_global_service_context
+from llama_index.llms import AzureOpenAI
+from llama_index.embeddings import OpenAIEmbedding
+import json
+import os
 from llama_index import SimpleDirectoryReader
 
 
-# Setup OpenAI key and initialize message history
-openai.api_key = st.secrets.openai_key
-st.header("Chat with the Streamlit docs 💬 📚")
+# Load config values
+with open(r'config.json') as config_file:
+    config_details = json.load(config_file)
+
+# Initialize message history
+st.header("Chat with André's research 💬 📚")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "assistant", "content": "Ask me a question about André's research!"}
     ]
 
 
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        documents = SimpleDirectoryReader(input_dir="./data", recursive=True).load_data()
+        llm = AzureOpenAI(
+            model="gpt-3.5-turbo",
+            engine="chatbot-streamlit",
+            temperature=0.5,
+            api_key=os.getenv("OPENAI_API_KEY"),
+            api_base=config_details['OPENAI_API_BASE'],
+            api_type="azure",
+            api_version=config_details['OPENAI_API_VERSION'],
+            system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."
+        )
+        # You need to deploy your own embedding model as well as your own chat completion model
+        embed_model = OpenAIEmbedding(
+            model="text-embedding-ada-002",
+            deployment_name="chatbot-streamlit-embedding",
+            api_key=os.getenv("OPENAI_API_KEY"),
+            api_base=config_details['OPENAI_API_BASE'],
+            api_type="azure",
+            api_version=config_details['OPENAI_API_VERSION'],
+        )
+        service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
+        set_global_service_context(service_context)
+        index = VectorStoreIndex.from_documents(documents) #, service_context=service_context)
         return index
 
 

59fea76144689d875480b02bc6818a640b5c8ac4
André Pedersen <andrped94@gmail.com>
2023-10-24 10:21:35 +0200

    Added TODO to download_test_data

--- chatbot/data.py
+++ chatbot/data.py
@@ -17,6 +17,8 @@ def download_test_data():
     # url = f"https://drive.google.com/drive/folders/uc?export=download&confirm=pbef&id={file_id}"
     url = "https://drive.google.com/drive/folders/1uDSAWtLvp1YPzfXUsK_v6DeWta16pq6y"
     with st.spinner(text="Downloading test data. This might take a minute."):
+        # @TODO: replace gown solution with a custom solution compatible with GitHub and 
+        # use st.progress to get more verbose during download
         download_folder(url=url, quiet=False, use_cookies=False, output="./data/")
 
 @st.cache_resource(show_spinner=False)
